a332cc57e3c065dd1572b91f6712a7a0292728ec
alexjbinnie <alexjbinnie@gmail.com>
2021-11-27 13:29:43 +0000

    Initial attempt

---
+++ rollabledie/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='RollableDie',
+    version='1.0.0',
+    url='https://github.com/alexjbinnie/codecov-test.git',
+    author='Alex Binnie',
+    author_email='',
+    description='',
+    packages=find_packages("src"),
+    install_requires=[],
+)
---
+++ rollabledie/tests/test_dice.py
@@ -0,0 +1,6 @@
+from rollabledie.dice import RollableDie
+
+
+def test_upper(self):
+    die = RollableDie()
+    self.assertEqual(die.sides, 6)
